3bccccf437fdd2321ce97ca68a8d18ef28bcb081
Beytullahzor7 <beytullahzor7@gmail.com>
2021-11-19 20:35:01 +0300

    Changes Updated

---
+++ onejavaVideo/src/onejavaVideo/Deneme.java
@@ -0,0 +1,19 @@
+package onejavaVideo;
+
+public class Deneme {
+
+	public static void main(String[] args) {
+		
+		for(int i = 5; i<=5; i--) {
+			for(int j = 1; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+			if(i == 0)
+				break;
+			
+		}
+
+	}
+
+}
